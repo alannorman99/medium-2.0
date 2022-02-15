93dc3390ff35d00099c7adae84643cb6098b5757
Alan Norman <asnorman3604@eagle.fgcu.edu>
2022-02-15 01:32:04 -0500

    Added section for comments on posts.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,12 +3,26 @@ import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 
 interface Props {
   post: Post
 }
 
 function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+
   return (
     <main>
       <Header />
@@ -37,7 +51,7 @@ function Post({ post }: Props) {
           </p>
         </div>
 
-        <div>
+        <div className="mt-10">
           <PortableText
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
@@ -61,6 +75,64 @@ function Post({ post }: Props) {
           />
         </div>
       </article>
+
+      <hr className="my-5 mx-auto max-w-lg border-yellow-500" />
+
+      <form className="my-10 mx-auto mb-10 flex max-w-2xl flex-col p-5">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="mt-2 py-3" />
+
+        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
+
+        <label className="mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register('name', { required: true })}
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="John Appleseed"
+            type="text"
+          />
+        </label>
+        <label className="mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register('email', { required: true })}
+            className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="John Appleseed"
+            type="text"
+          />
+        </label>
+        <label className="mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register('comment', { required: true })}
+            className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+            placeholder="John Appleseed"
+            rows={8}
+          />
+        </label>
+        {/* errors will return when form validation fails */}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500"> - The Name Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              {' '}
+              - The Comment Field is required
+            </span>
+          )}
+          {errors.email && (
+            <span className="text-red-500"> - The Email Field is required</span>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="focus:shadow-outline focuse:outline-none cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400"
+        />
+      </form>
     </main>
   )
 }
